09d4eee1c43c31e5b49c585cd0c17ba90515126d
klukas17 <mihael.milicevic3@gmail.com>
2022-03-13 11:12:16 +0100

    added check-optimistic and check-consistent

--- lab1cpp/solution.cpp
+++ lab1cpp/solution.cpp
@@ -11,15 +11,24 @@
 #include <float.h>
 #include "Node.h"
 
+/*
+ * TODO:
+ *      improve input reading
+ *      improve check-optimistic
+ *      run autograder
+ */
+
 std::string alg = "", ss = "", h = "";
 bool check_optimistic = false, check_consistent = false, ss_provided = false, h_provided = false;
 
 std::string initial_state;
+std::set<std::string> states;
 std::set<std::string> goal_states;
 std::map<std::string, std::vector<std::pair<std::string, double>>> successor_function;
 std::map<std::string, double> heuristic_function;
 
-std::map<std::string, std::map<std::string, double>> Dijkstra_map;
+std::map<std::string, double> distance;
+std::map<std::string, double> distances;
 
 void read_arguments(int argc, char* argv[]) {
     int index = 1;
@@ -56,6 +65,7 @@ void read_ss() {
     std::ifstream input(ss);
     std::string line;
     bool initial = false, goal = false;
+    int l = 2;
     for (;getline(input, line);) {
         if (line[0] == '#') continue;
         if (!initial) {
@@ -78,6 +88,7 @@ void read_ss() {
             std::regex_match(const_cast<char*>(line.c_str()), match, r1);
             left_side = match[1];
             right_side = match[2];
+            states.insert(left_side);
             while (right_side != "") {
                 std::regex_match(const_cast<char*>(right_side.c_str()), match, r2);
                 state = match[2];
@@ -87,6 +98,8 @@ void read_ss() {
             }
             std::sort(successor_function[left_side].begin(), successor_function[left_side].end(),
                  [] (std::pair<std::string, double> i1, std::pair<std::string, double> i2) {return (i1.first < i2.first);});
+            l++;
+            if (l % 1000 == 0) std::cout << l << std::endl;
         }
     }
 }
@@ -94,6 +107,7 @@ void read_ss() {
 void read_h() {
     std::ifstream input(h);
     std::string line;
+    int l = 0;
     for (;getline(input, line);) {
         if (line[0] == '#') continue;
         std::regex r3("([A-Za-zšđčćžŠĐČĆŽ0-9_]*): ([0-9]*[\\.[0-9]+]?)");
@@ -101,6 +115,8 @@ void read_h() {
         std::regex_match(const_cast<char*>(line.c_str()), match, r3);
         std::string left_side = match[1], right_side = match[2];
         heuristic_function[left_side] = std::stod(right_side);
+        l++;
+        if (l % 1000 == 0) std::cout << l << std::endl;
     }
 }
 
@@ -152,12 +168,17 @@ void print_results(bool found, std::set<std::string> visited, Node* result) {
 
 void bfs() {
 
+    std::vector<Node*> all_nodes;
+
     std::queue<Node*> open;
     std::set<std::string> visited;
     bool found = false;
     Node* result = nullptr;
 
-    open.push(new Node(initial_state, nullptr, 0));
+    Node* start_node = new Node(initial_state, nullptr, 0);
+    all_nodes.push_back(start_node);
+    open.push(start_node);
+
     while (open.size() > 0) {
         Node* n = open.front();
         open.pop();
@@ -168,15 +189,25 @@ void bfs() {
             break;
         }
         for (std::pair<std::string, double> m : successor_function[n->state]) {
-            if (visited.find(m.first) == visited.end()) open.push(new Node(m.first, n, n->cost + m.second));
+            if (visited.find(m.first) == visited.end()) {
+                Node* new_node = new Node(m.first, n, n->cost + m.second);
+                all_nodes.push_back(new_node);
+                open.push(new_node);
+            }
         }
     }
 
     std::cout << "# BFS" << std::endl;
     print_results(found, visited, result);
+
+    for (auto n : all_nodes)
+        n->~Node();
 }
 
-void ucs() {
+double ucs(bool print, std::string start) {
+
+    std::vector<Node*> all_nodes;
+
     auto comparator = [] (Node* a, Node* b) {
         if (a->cost != b->cost) return (a->cost > b->cost);
         else return (a->state > b->state);
@@ -187,7 +218,10 @@ void ucs() {
     bool found = false;
     Node* result = nullptr;
 
-    open.push(new Node(initial_state, nullptr, 0));
+    Node* start_node = new Node(start, nullptr, 0);
+    all_nodes.push_back(start_node);
+    open.push(start_node);
+
     while (open.size() > 0) {
         Node* n = open.top();
         open.pop();
@@ -198,15 +232,31 @@ void ucs() {
             break;
         }
         for (std::pair<std::string, double> m : successor_function[n->state]) {
-            if (visited.find(m.first) == visited.end()) open.push(new Node(m.first, n, n->cost + m.second));
+            if (visited.find(m.first) == visited.end()) {
+                Node* new_node = new Node(m.first, n, n->cost + m.second);
+                all_nodes.push_back(new_node);
+                open.push(new_node);
+            }
         }
     }
 
-    std::cout << "# UCS" << std::endl;
-    print_results(found, visited, result);
+    double ret_val = result->cost;
+
+    if (print) {
+        std::cout << "# UCS" << std::endl;
+        print_results(found, visited, result);
+    }
+
+    for (auto n : all_nodes)
+        n->~Node();
+
+    return ret_val;
 }
 
 void astar() {
+
+    std::vector<Node*> all_nodes;
+
     auto comparator = [] (Node* a, Node* b) {
         if (a->f != b->f) return (a->f > b->f);
         else return (a->f > b->f);
@@ -219,11 +269,14 @@ void astar() {
     std::set<Node*> closed;
     std::map<std::string, double> front_map;
 
-    for (auto it = heuristic_function.begin(); it != heuristic_function.end(); it++)
-        front_map[it->first] = DBL_MAX;
+    for (auto x : states)
+        front_map[x] = DBL_MAX;
 
     front_map[initial_state] = 0;
-    open.push(new Node(initial_state, nullptr, 0));
+
+    Node* start_node = new Node(initial_state, nullptr, 0);
+    all_nodes.push_back(start_node);
+    open.push(start_node);
 
     while (open.size() > 0) {
         Node* n = open.top();
@@ -237,12 +290,12 @@ void astar() {
         closed.insert(n);
         for (std::pair<std::string, double> u : successor_function[n->state]) {
             Node* m = new Node(u.first, n, n->cost + u.second);
+            all_nodes.push_back(m);
             bool flag = false;
             for (Node* m_ : closed) {
                 if (m_->state == m->state) {
                     if (m_->cost < m->cost) {
                         flag = true;
-                        m->~Node();
                         break;
                     }
                     else {
@@ -253,7 +306,6 @@ void astar() {
             if (flag) continue;
             if (front_map[m->state] < m->cost) {
                 flag = true;
-                m->~Node();
             }
             front_map[m->state] = m->cost;
             m->f = m->cost + heuristic_function[m->state];
@@ -263,21 +315,120 @@ void astar() {
 
     std::cout << "# A-STAR "<< h << std::endl;
     print_results(found, visited, result);
+
+    for (auto n : all_nodes)
+        n->~Node();
 }
 
 void Dijkstra() {
-    std::map<std::string, double> dist;
-    std::map<std::string, std::string> prev;
 
-    auto comparator = [&dist] (std::string a, std::string b) {
-        return (dist[a] < dist[b]);
-    };
-    std::priority_queue<std::string, std::vector<std::string>, decltype(comparator)> Q(comparator);
+    std::map<std::string, std::map<std::string, double>> Dijkstra_map;
 
-    for (auto it = heuristic_function.begin(); it != heuristic_function.end(); it++) {
-        dist[it.first] = DBL_MAX;
-        prev[it.first] = nullptr;
+    for (auto it = goal_states.begin(); it != goal_states.end(); it++) {
+        std::string goal_state = *it;
+        std::map<std::string, double> dist;
+        std::map<std::string, bool> finished;
+
+        auto comparator = [] (std::pair<std::string, double> a, std::pair<std::string, double> b) {
+            return (a.second > b.second);
+        };
+        std::priority_queue<std::pair<std::string, double>, std::vector<std::pair<std::string, double>>, decltype(comparator)> Q(comparator);
+
+        for (auto x : states) {
+            dist[x] = DBL_MAX;
+        }
+
+        dist[goal_state] = 0;
+        Q.push({goal_state, 0});
+
+        while (Q.size() > 0) {
+            auto u = Q.top();
+            Q.pop();
+            if (finished[u.first] && dist[u.first] < u.second) continue;
+            finished[u.first] = true;
+            dist[u.first] = u.second;
+            for (auto neighbor : successor_function[u.first]) {
+                auto v = neighbor.first;
+                auto d = neighbor.second;
+                double alt = u.second + d;
+                if (alt < dist[v]) {
+                    Q.push({v, alt});
+                }
+            }
+        }
+
+        Dijkstra_map[goal_state] = dist;
     }
+
+    for (auto x : states) {
+        distances[x] = DBL_MAX;
+    }
+
+    for (auto it = Dijkstra_map.begin(); it != Dijkstra_map.end(); it++) {
+        auto x = (*it).second;
+        for (auto pair : x) {
+            auto y = pair.first;
+            auto z = pair.second;
+            if (distances[y] > z) {
+                distances[y] = z;
+            }
+        }
+    }
+
+}
+
+void calculate_distance_ucs() {
+    for (auto state : states)
+        distance[state] = ucs(false, state);
+}
+
+void check_heuristic_optimistic() {
+    std::cout << "# HEURISTIC-OPTIMISTIC "<< h << std::endl;
+    bool optimistic = true;
+
+    for (auto pair : heuristic_function) {
+        auto state = pair.first;
+        auto heuristic_cost = pair.second;
+        auto real_cost = distance[state];
+        if (heuristic_cost <= real_cost)
+            std::cout << "[CONDITION]: [OK] h(" << state << ") <= h*: " << heuristic_cost << " <= " << real_cost << std::endl;
+        else {
+            std::cout << "[CONDITION]: [ERR] h(" << state << ") <= h*: " << heuristic_cost << " <= " << real_cost << std::endl;
+            optimistic = false;
+        }
+    }
+
+    if (optimistic)
+        std::cout << "[CONCLUSION]: Heuristic is optimistic." << std::endl;
+    else
+        std::cout << "[CONCLUSION]: Heuristic is not optimistic." << std::endl;
+}
+
+void check_heuristic_consistent() {
+    std::cout << "# HEURISTIC-CONSISTENT "<< h << std::endl;
+    bool consistent = true;
+
+    for (auto pair : successor_function) {
+        auto state = pair.first;
+        auto neighbours = pair.second;
+        for (auto m : neighbours) {
+            auto n = m.first;
+            auto c = m.second;
+            auto h1 = heuristic_function[state];
+            auto h2 = heuristic_function[n];
+            if (h1 <= h2 + c)
+                std::cout << "[CONDITION]: [OK] h(" << state << ") <= h(" << n << ") + c: " << h1 << " <= " << h2 << " + " << c << std::endl;
+            else {
+                std::cout << "[CONDITION]: [ERR] h(" << state << ") <= h(" << n << ") + c: " << h1 << " <= " << h2 << " + " << c << std::endl;
+                consistent = false;
+            }
+        }
+    }
+
+    if (consistent)
+        std::cout << "[CONCLUSION]: Heuristic is consistent." << std::endl;
+    else
+        std::cout << "[CONCLUSION]: Heuristic is not consistent." << std::endl;
 }
 
 int main(int argc, char* argv[]){
@@ -289,7 +440,12 @@ int main(int argc, char* argv[]){
     //print_heuristic_function();
     std::cout << std::setprecision(1) << std::fixed;
     if (alg == "bfs") bfs();
-    else if (alg == "ucs") ucs();
+    else if (alg == "ucs") ucs(true, initial_state);
     else if (alg == "astar") astar();
-    if (check_consistent || check_optimistic) Dijkstra();
+    if (check_optimistic){
+        calculate_distance_ucs();
+        check_heuristic_optimistic();
+    };
+    if (check_consistent)
+        check_heuristic_consistent();
 }
